8999e111aaaf11ac8772d0b7a910095ed0416456
Kyle Thornton <kylemthornton@gmail.com>
2023-11-01 15:14:33 -0700

    Add search bar, make container

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,14 +2,19 @@ import link from 'next/link'
 
 export default function NavBar() {
     return (
-        <nav className='flex justify-between p-10 bg-blue-100'>
-            <p>logo</p>
-            <ul className='flex cursor-pointer'>
-                <li className='px-2 hover:font-bold'>Movies</li>
-                <li className='px-2 hover:font-bold'>TV</li>
-                <li className='px-2 hover:font-bold'>People</li>
-                <li className='px-2 hover:font-bold'>Dark/Light</li>
-            </ul>
-        </nav>
+        <div className='bg-blue-100'>
+            <nav className='container flex justify-between p-8 items-center'>
+                <ul className='flex cursor-pointer'>
+                    <li className='px-2 hover:font-bold'>Movies</li>
+                    <li className='px-2 hover:font-bold'>TV</li>
+                    <li className='px-2 hover:font-bold'>People</li>
+                    <li className='px-2 hover:font-bold'>Dark/Light</li>
+                </ul>
+                <div className='flex join'>
+                    <input type="text" className='input input-bordered join-item w-80' />
+                    <button className='btn join-item'>Search</button>
+                </div>
+            </nav>
+        </div>
     )
 }
