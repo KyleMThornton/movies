2163db2bfdbce0fa50658ccc740ed64e45e44137
Kyle Thornton <kylemthornton@gmail.com>
2023-11-03 12:51:22 -0700

    Add scale on hover to movie posters

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -10,13 +10,13 @@ type MovieCardProps = {
 export const MovieCard: React.FC<MovieCardProps> = ({ poster, title, releaseDate, voteAverage }) => {
     return (
         <div className="flex flex-col m-2 w-96 h-96">
-            <div className="h-60 w-48">
+            <div className="h-60 w-48 hover:scale-105 transition-all duration-200 ease-in-out cursor-pointer">
                 <img src={`https://image.tmdb.org/t/p/original/${poster}`} alt={title} className="w-full h-full object-cover rounded-lg" />
             </div>
             <div className="flex flex-col">
                 <h3 className="pt-5 font-bold text-md">{title}</h3>
                 <div className="flex flex-col">
-                    <p>{releaseDate}</p>
+                    <p>{releaseDate.slice(5,7)}/{releaseDate.slice(8,10)}/{releaseDate.slice(0,4)}</p>
                     <p>{voteAverage}/10</p>
                 </div>
             </div>
