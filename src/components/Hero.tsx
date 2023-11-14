2b3a9fb87818142b61080ee96ae346675f70a95c
Kyle Thornton <kylemthornton@gmail.com>
2023-11-13 18:52:28 -0800

    Create hero section

---
+++ src/components/Hero.tsx
@@ -0,0 +1,12 @@
+export default async function Hero() {
+
+    return (
+        <div>
+            <div className="flex flex-col items-center justify-center h-96 bg-gray-900">
+                <div className="flex flex-col items-center justify-center w-1/2 h-1/2">
+                    <h1 className="text-6xl font-bold text-white">Welcome to the Movie Database</h1>
+                </div>
+            </div>
+        </div>
+    ) 
+}
