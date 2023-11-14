9b119d7daa2e2076da6f0c80555406538b27a5e4
Kyle Thornton <kylemthornton@gmail.com>
2023-11-14 09:03:36 -0800

    Photo loading

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,11 @@
 export default async function Hero() {
+    const response = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.TMDB_API_KEY}`)
+    const movieData = await response.json()
+    console.log(movieData)
 
     return (
         <div>
+            <img src={`https://image.tmdb.org/t/p/original${movieData.results[0].backdrop_path}`} alt="" className="h-96" />
             <div className="flex flex-col items-center justify-center h-96 bg-gray-900">
                 <div className="flex flex-col items-center justify-center w-1/2 h-1/2">
                     <h1 className="text-6xl font-bold text-white">Welcome to the Movie Database</h1>
