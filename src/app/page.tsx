10533d48434ff974963eab32f208128df1ca4a82
Kyle Thornton <kylemthornton@gmail.com>
2023-11-13 18:52:34 -0800

    Add hero to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Hero from "@/components/Hero";
 import Movies from "@/components/Movies";
 import TV from "@/components/TV";
 
 export default function Home() {
   return (
     <main>
+      <Hero />
       <Movies />
       <TV />
     </main>
