a3614caeed1b6c909d4b999aae81b8b79f357371
Kyle Thornton <kylemthornton@gmail.com>
2023-11-15 14:37:57 -0800

    Add bg image

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,10 +5,14 @@ export default async function Hero() {
 
     return (
         <div>
-            <img src={`https://image.tmdb.org/t/p/original${movieData.results[0].backdrop_path}`} alt="" className="h-96" />
-            <div className="flex flex-col items-center justify-center h-96 bg-gray-900">
-                <div className="flex flex-col items-center justify-center w-1/2 h-1/2">
-                    <h1 className="text-6xl font-bold text-white">Welcome to the Movie Database</h1>
+            {/* <img src={`https://image.tmdb.org/t/p/original${movieData.results[0].backdrop_path}`} alt="" className="h-96" /> */}
+            <div className="bg-slate-950">
+                <div className="bg-cover bg-center h-96 opacity-50" style={{backgroundImage: `url(https://image.tmdb.org/t/p/original${movieData.results[0].backdrop_path})`}}>
+                    <div className="container">
+                        <div className="flex flex-col items-center justify-center w-1/2 h-1/2">
+                            <h1 className="text-6xl font-bold text-white">Welcome to the Movie Database</h1>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
