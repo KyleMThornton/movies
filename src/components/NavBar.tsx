bb52b853188ebe499a6642752d17c644629b25e8
Kyle Thornton <kylemthornton@gmail.com>
2023-11-15 14:38:14 -0800

    Small screen fix

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,16 +2,16 @@ import link from 'next/link'
 
 export default function NavBar() {
     return (
-        <div className='bg-blue-100'>
-            <nav className='container flex justify-between p-8 items-center'>
-                <ul className='flex cursor-pointer'>
-                    <li className='px-2 hover:font-bold'>Movies</li>
-                    <li className='px-2 hover:font-bold'>TV</li>
-                    <li className='px-2 hover:font-bold'>People</li>
-                    <li className='px-2 hover:font-bold'>Dark/Light</li>
+        <div className='bg-slate-800'>
+            <nav className='container flex flex-col md:flex-row justify-between p-8 items-center text-slate-50'>
+                <ul className='flex cursor-pointer pb-5 md:pb-0'>
+                    <li className='px-2 text-xl hover:font-bold'>Movies</li>
+                    <li className='px-2 text-xl hover:font-bold'>TV</li>
+                    <li className='px-2 text-xl hover:font-bold'>People</li>
+                    <li className='px-2 text-xl hover:font-bold'>Dark/Light</li>
                 </ul>
                 <div className='flex join'>
-                    <input type="text" className='input input-bordered join-item w-80' />
+                    <input type="text" className='input input-bordered join-item wd-1/3 md:w-80' />
                     <button className='btn join-item'>Search</button>
                 </div>
             </nav>
