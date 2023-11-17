dce5861cf5d8fe696554f617d0b92bfc60105c9b
Kyle Thornton <kylemthornton@gmail.com>
2023-11-16 20:47:23 -0800

    Fix hero image

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,19 +1,14 @@
 export default async function Hero() {
     const response = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.TMDB_API_KEY}`)
     const movieData = await response.json()
-    console.log(movieData)
 
     return (
-        <div>
-            {/* <img src={`https://image.tmdb.org/t/p/original${movieData.results[0].backdrop_path}`} alt="" className="h-96" /> */}
-            <div className="bg-slate-950">
-                <div className="bg-cover bg-center h-96 opacity-50" style={{backgroundImage: `url(https://image.tmdb.org/t/p/original${movieData.results[0].backdrop_path})`}}>
-                    <div className="container">
-                        <div className="flex flex-col items-center justify-center w-1/2 h-1/2">
-                            <h1 className="text-6xl font-bold text-white">Welcome to the Movie Database</h1>
-                        </div>
-                    </div>
-                </div>
+        <div className="relative bg-slate-950 h-96">
+            <div className="absolute inset-0 bg-cover bg-center h-96 opacity-50" style={{backgroundImage: `url(https://image.tmdb.org/t/p/original${movieData.results[0].backdrop_path})`}}>
+            </div>
+            <div className="container relative flex flex-col justify-center items-center text-center h-full p-5">
+                <h1 className="text-6xl font-bold text-white">Welcome to the Movie Database!</h1>
+                <h2 className="text-xl text-white">Get info on your favorite movies, tv shows, and actors!</h2>
             </div>
         </div>
     ) 
