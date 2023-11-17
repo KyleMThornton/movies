bbf3195a2e822bfbdfe6a1b81c33c468cdf09101
Kyle Thornton <kylemthornton@gmail.com>
2023-11-16 21:01:38 -0800

    Add color rating system

--- src/components/Rating.tsx
+++ src/components/Rating.tsx
@@ -1,9 +1,18 @@
 export default function Rating({ review }: { review: number }) {
     const ratingPercentage = review * 10
+    let ratingColor = ""
+
+    if(ratingPercentage >= 75) {
+        ratingColor = "text-green-600"
+    } else if(ratingPercentage >= 40 && ratingPercentage < 75) {
+        ratingColor = "text-yellow-600"
+    } else {
+        ratingColor = "text-red-600"
+    }
 
     return (
         <>
-            <div className="radial-progress text-blue-600" style={{"--value":ratingPercentage, "--size": "2rem"}}>{ratingPercentage}</div>
+            <div className={`radial-progress ${ratingColor}`} style={{"--value":ratingPercentage, "--size": "2.5rem"}}>{ratingPercentage}</div>
         </>
     )
 }
