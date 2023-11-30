a5734e8b267393a5f7f7bcf302a9836f20dbc512
Kyle Thornton <kylemthornton@gmail.com>
2023-11-29 19:13:57 -0800

    Fix navbar text color

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,8 +3,8 @@ import link from 'next/link'
 export default function NavBar() {
     return (
         <div className='bg-slate-800'>
-            <nav className='container flex flex-col md:flex-row justify-between p-8 items-center text-slate-50'>
-                <ul className='flex cursor-pointer pb-5 md:pb-0'>
+            <nav className='container flex flex-col md:flex-row justify-between p-8 items-center'>
+                <ul className='flex cursor-pointer pb-5 md:pb-0 text-slate-50'>
                     <li className='px-2 text-xl hover:font-bold'>Movies</li>
                     <li className='px-2 text-xl hover:font-bold'>TV</li>
                     <li className='px-2 text-xl hover:font-bold'>People</li>
