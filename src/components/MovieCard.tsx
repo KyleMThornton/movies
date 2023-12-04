1c9ef906f3961c061ff10e2a78b288e800210efa
Kyle Thornton <kylemthornton@gmail.com>
2023-12-04 15:51:08 -0800

    Add link to details page and added id prop

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,17 +1,19 @@
 import Rating from "./Rating";
+import Link from 'next/link'
 
 type MovieCardProps = {
     poster: string;
     title: string;
     releaseDate: string;
     voteAverage: number;
+    id: number;
 };
 
-export const MovieCard: React.FC<MovieCardProps> = ({ poster, title, releaseDate, voteAverage }) => {
+export const MovieCard: React.FC<MovieCardProps> = ({ poster, title, releaseDate, voteAverage, id }) => {
     return (
         <div className="flex flex-col m-2 w-96 h-96">
             <div className="h-60 w-48 hover:scale-105 transition-all duration-200 ease-in-out cursor-pointer">
-                <img src={`https://image.tmdb.org/t/p/original/${poster}`} alt={title} className="w-full h-full object-cover rounded-lg" />
+                <Link href={`/movie/${id}`}><img src={`https://image.tmdb.org/t/p/original/${poster}`} alt={title} className="w-full h-full object-cover rounded-lg" /></Link>
             </div>
             <div className="flex flex-col">
                 <h3 className="pt-5 font-bold text-md">{title}</h3>
