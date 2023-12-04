b0790965d17f460bc025cb45ac31e328783a693d
Kyle Thornton <kylemthornton@gmail.com>
2023-12-04 15:50:26 -0800

    Update params for movie data

---
+++ src/app/movie/[id]/page.tsx
@@ -0,0 +1,18 @@
+async function getMovieData(movieId: number) {
+    const response = await fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${process.env.TMDB_API_KEY}`)
+    const movieData = await response.json()
+    return movieData
+}
+
+export default async function Movie({params } : { params: {movie: number}}) {
+    const movieData = await getMovieData(params.movie) 
+
+    return (
+        <div className="container">
+            <div className="h-60 w-48 hover:scale-105 transition-all duration-200 ease-in-out cursor-pointer">
+                <img src={`https://image.tmdb.org/t/p/original/${movieData.poster_path}`} alt={movieData.title} className="w-full h-full object-cover rounded-lg" />
+            </div>
+            <h2>{movieData.title}</h2>
+        </div>
+    )
+}
