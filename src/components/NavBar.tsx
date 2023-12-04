54e82d0b338a45e9229d232cf122dfa7ea63763e
Kyle Thornton <kylemthornton@gmail.com>
2023-12-04 15:51:24 -0800

    Add home button

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,13 @@
-import link from 'next/link'
+import Link from 'next/link';
+import { FaHome } from "react-icons/fa";
+
 
 export default function NavBar() {
     return (
         <div className='bg-slate-800'>
             <nav className='container flex flex-col md:flex-row justify-between p-8 items-center'>
-                <ul className='flex cursor-pointer pb-5 md:pb-0 text-slate-50'>
+                <ul className='flex cursor-pointer pb-5 md:pb-0 text-slate-50 items-center'>
+                    <span className='px-2 text-xl'><Link href="/"><FaHome /></Link></span>
                     <li className='px-2 text-xl hover:font-bold'>Movies</li>
                     <li className='px-2 text-xl hover:font-bold'>TV</li>
                     <li className='px-2 text-xl hover:font-bold'>People</li>
