67c6d045674d567801a7efb7972832923cd6d971
Kyle Thornton <kylemthornton@gmail.com>
2023-12-06 21:12:21 -0800

    Create check for null image path

--- src/components/ActorCard.tsx
+++ src/components/ActorCard.tsx
@@ -6,10 +6,11 @@ type ActorCardProps = {
 };
 
 export const ActorCard: React.FC<ActorCardProps> = ({name, character, profile_path, id}) => {
+    const headshot = profile_path ? `https://image.tmdb.org/t/p/original/${profile_path}` : "/images/noheadshot.jpg"
     return (
             <div className="flex flex-col m-2 h-80 text-black border rounded-xl">
                 <div className="h-60 w-48 hover:scale-105 transition-all duration-200 ease-in-out cursor-pointer">
-                    <img src={`https://image.tmdb.org/t/p/original/${profile_path}`} alt={name} className="w-full h-full object-cover rounded-lg" />
+                    <img src={headshot} alt={name} className="w-full h-full object-cover rounded-lg" />
                 </div>
                 <div className="flex flex-col">
                     <h3 className="pt-2 pl-2 font-bold text-md">{name}</h3>
