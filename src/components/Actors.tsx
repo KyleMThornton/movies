27c01625b00c469ece88d7c64b79287ddba7cb82
Kyle Thornton <kylemthornton@gmail.com>
2023-12-07 15:01:04 -0800

    Add error catching

--- src/components/Actors.tsx
+++ src/components/Actors.tsx
@@ -10,20 +10,22 @@ export default async function Actors(movieId:any) {
     const actorData = await getActorData(movieId.movieId)
 
     return (
-        <div className="h-112">
-            <h1 className="text-3xl font-bold py-5">Actors</h1>
-            <div className="flex overflow-auto w-full custom-scrollbar">
-                {actorData.cast.map((actor: any) => (
-                    <ActorCard
-                        key={actor.id}
-                        id={actor.id}
-                        name={actor.name}
-                        character={actor.character}
-                        profile_path={actor.profile_path}
-                    />
-                )
-                )}
-            </div>
-        </div>
+        <> {actorData.cast.length !== 0 ?
+            <div className="h-112">
+                <h1 className="text-3xl font-bold py-5">Actors</h1>
+                <div className="flex overflow-auto w-full custom-scrollbar">
+                    {actorData.cast.map((actor: any) => (
+                        <ActorCard
+                            key={actor.id}
+                            id={actor.id}
+                            name={actor.name}
+                            character={actor.character}
+                            profile_path={actor.profile_path}
+                        />
+                    )
+                    )}
+                </div>
+            </div> : null}
+        </>
     )
 }
