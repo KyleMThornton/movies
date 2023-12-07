d2b3a01bbc8edd81a37c4a0880a5e37697d2183d
Kyle Thornton <kylemthornton@gmail.com>
2023-12-06 21:11:45 -0800

    Restyle rating spacing

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -15,16 +15,13 @@ export const MovieCard: React.FC<MovieCardProps> = ({ poster, title, releaseDate
             <div className="h-60 w-48 hover:scale-105 transition-all duration-200 ease-in-out cursor-pointer">
                 <Link href={`/movie/${id}`}><img src={`https://image.tmdb.org/t/p/original/${poster}`} alt={title} className="w-full h-full object-cover rounded-lg" /></Link>
             </div>
-            <div className="flex flex-col">
-                <h3 className="pt-5 font-bold text-md">{title}</h3>
-                <div className="flex items-center justify-between">
-                    <div className="flex flex-col">
-                        <p>{releaseDate.slice(5,7)}/{releaseDate.slice(8,10)}/{releaseDate.slice(0,4)}</p>
-                        <p>{voteAverage}/10</p>
-                    </div>
-                    <div className="justify-items-end">
-                        <Rating review={voteAverage} />
-                    </div>
+            <div className="flex items-center justify-between pt-5">
+                <div>
+                    <h3 className="font-bold text-md">{title}</h3>
+                    <p>{releaseDate.slice(5,7)}/{releaseDate.slice(8,10)}/{releaseDate.slice(0,4)}</p>
+                </div>
+                <div className="">
+                        {voteAverage ? <Rating review={voteAverage} /> : null}
                 </div>
             </div>
         </div>
