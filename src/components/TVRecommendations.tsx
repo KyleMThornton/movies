46713336c53cbbeadbc8e48b94bae54258ae08c8
Kyle Thornton <kylemthornton@gmail.com>
2023-12-07 15:02:46 -0800

    Create TVRecs component

---
+++ src/components/TVRecommendations.tsx
@@ -0,0 +1,32 @@
+import { MovieCard } from "./MovieCard"
+
+async function getTVRecommendations(TVId: number) {
+    const response = await fetch(`https://api.themoviedb.org/3/TV/${TVId}/recommendations?api_key=${process.env.TMDB_API_KEY}`)
+    const TVRecs = await response.json()
+    return TVRecs
+}
+
+export default async function TVRecommendations(TVId: any) {
+    const TVRecs = await getTVRecommendations(TVId.TVId)
+
+    return (
+        <> {TVRecs.results.length !== 0 ?
+            <div className="container text-black dark:text-white pb-5">
+                <h2 className="text-4xl font-bold p-2">Recommendations</h2>
+                <div className="flex overflow-auto w-full custom-scrollbar">
+                    {TVRecs.results.map((TV: any) => (
+                        <MovieCard
+                            key={TV.id}
+                            id={TV.id}
+                            poster={TV.poster_path}
+                            title={TV.title}
+                            releaseDate={TV.release_date}
+                            voteAverage={TV.vote_average}
+                        />
+                    )
+                    )}
+                </div>
+            </div> : null}
+        </>
+    )
+}
