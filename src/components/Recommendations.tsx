e1acb1a444b466834a890c82c236454a3cc49abe
Kyle Thornton <kylemthornton@gmail.com>
2023-12-06 21:11:31 -0800

    Create Recommendations component

---
+++ src/components/Recommendations.tsx
@@ -0,0 +1,30 @@
+import { MovieCard } from "./MovieCard"
+
+async function getRecommendations(movieId: number) {
+    const response = await fetch(`https://api.themoviedb.org/3/movie/${movieId}/recommendations?api_key=${process.env.TMDB_API_KEY}`)
+    const movieRecs = await response.json()
+    return movieRecs
+}
+
+export default async function Recommendations(movieId: any) {
+    const movieRecs = await getRecommendations(movieId.movieId)
+
+    return (
+        <div className="container text-black dark:text-white">
+            <h2 className="text-4xl font-bold p-2">Recommendations</h2>
+            <div className="flex overflow-auto w-full custom-scrollbar">
+                {movieRecs.results.map((movie: any) => (
+                    <MovieCard
+                        key={movie.id}
+                        id={movie.id}
+                        poster={movie.poster_path}
+                        title={movie.title}
+                        releaseDate={movie.release_date}
+                        voteAverage={movie.vote_average}
+                    />
+                )
+                )}
+            </div>
+        </div>
+    )
+}
