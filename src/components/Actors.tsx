cfb57a49ae4c7f0f8ddf5f272582ede76eea7d46
Kyle Thornton <kylemthornton@gmail.com>
2023-12-06 16:52:26 -0800

    Create Actors component

---
+++ src/components/Actors.tsx
@@ -0,0 +1,29 @@
+import { ActorCard } from "./ActorCard"
+
+async function getActorData(movieId: number) {
+    const response = await fetch(`https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=${process.env.TMDB_API_KEY}`)
+    const actorData = await response.json()
+    return actorData
+}
+
+export default async function Actors(movieId:any) {
+    const actorData = await getActorData(507089)
+
+    return (
+        <div className="h-128">
+            <h1 className="text-3xl font-bold py-5">Actors</h1>
+            <div className="flex overflow-auto w-full custom-scrollbar">
+                {actorData.cast.map((actor: any) => (
+                    <ActorCard
+                        key={actor.id}
+                        id={actor.id}
+                        name={actor.name}
+                        character={actor.character}
+                        profile_path={actor.profile_path}
+                    />
+                )
+                )}
+            </div>
+        </div>
+    )
+}
