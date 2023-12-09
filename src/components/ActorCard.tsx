dc41a6392b7ae4c5f432f917e864c2e5edbc26d2
Kyle Thornton <kylemthornton@gmail.com>
2023-12-09 14:38:19 -0800

    Link actor card to person page

--- src/components/ActorCard.tsx
+++ src/components/ActorCard.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 type ActorCardProps = {
     name: string;
     character: string;
@@ -10,7 +12,7 @@ export const ActorCard: React.FC<ActorCardProps> = ({name, character, profile_pa
     return (
             <div className="flex flex-col m-2 h-80 text-black border rounded-xl">
                 <div className="h-60 w-48 hover:scale-105 transition-all duration-200 ease-in-out cursor-pointer">
-                    <img src={headshot} alt={name} className="w-full h-full object-cover rounded-lg" />
+                    <Link href={`/person/${id}`}><img src={headshot} alt={name} className="w-full h-full object-cover rounded-lg" /></Link>
                 </div>
                 <div className="flex flex-col">
                     <h3 className="pt-2 pl-2 font-bold text-md">{name}</h3>
