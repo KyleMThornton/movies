a825ec9f4ec8c023909ed2f17560c219f4018c29
Kyle Thornton <kylemthornton@gmail.com>
2023-12-12 18:29:30 -0800

    Add links to search page

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -8,9 +8,9 @@ export default function NavBar() {
             <nav className='container flex flex-col md:flex-row justify-between p-8 items-center'>
                 <ul className='flex cursor-pointer pb-5 md:pb-0 text-slate-50 items-center'>
                     <span className='px-2 text-xl'><Link href="/"><FaHome /></Link></span>
-                    <li className='px-2 text-xl hover:font-bold'>Movies</li>
-                    <li className='px-2 text-xl hover:font-bold'>TV</li>
-                    <li className='px-2 text-xl hover:font-bold'>People</li>
+                    <Link href="/search/movie"><li className='px-2 text-xl hover:font-bold'>Movies</li></Link>
+                    <Link href="/search/tv"><li className='px-2 text-xl hover:font-bold'>TV</li></Link>
+                    <Link href="/search/person"><li className='px-2 text-xl hover:font-bold'>People</li></Link>
                     <li className='px-2 text-xl hover:font-bold'>Dark/Light</li>
                 </ul>
                 <div className='flex join'>
