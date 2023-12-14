15071e189b408cb154e16a0c48f732ca36e6a2e4
Kyle Thornton <kylemthornton@gmail.com>
2023-12-14 10:36:34 -0800

    Add conditionals based on where card is rendered

--- src/components/ActorCard.tsx
+++ src/components/ActorCard.tsx
@@ -5,23 +5,38 @@ type ActorCardProps = {
     character?: string;
     profile_path: string;
     id: number;
+    known_for?: Array<string>;
 };
 
-export const ActorCard: React.FC<ActorCardProps> = ({name, character, profile_path, id}) => {
+export const ActorCard: React.FC<ActorCardProps> = ({name, character, profile_path, id, known_for}) => {
     const headshot = profile_path ? `https://image.tmdb.org/t/p/original/${profile_path}` : "/images/noheadshot.jpg"
     return (
-            <div className="flex flex-col m-2 h-80 text-black border rounded-xl">
-                <div className="h-60 w-48 hover:scale-105 transition-all duration-200 ease-in-out cursor-pointer">
-                    <Link href={`/person/${id}`}><img src={headshot} alt={name} className="w-full h-full object-cover rounded-lg" /></Link>
-                </div>
-                <div className="flex flex-col">
-                    <h3 className="pt-2 pl-2 font-bold text-md">{name}</h3>
-                    <div className="flex items-center justify-between pl-2">
-                        {character ? <div className="flex flex-col">
-                            <p>{character}</p>
-                        </div> : null}
-                    </div>
-                </div>
-            </div>
-    )
+      <div className="flex flex-col m-2 h-auto text-black border rounded-xl pb-2">
+        <div className="h-60 w-48 hover:scale-105 transition-all duration-200 ease-in-out cursor-pointer">
+          <Link href={`/person/${id}`}>
+            <img
+              src={headshot}
+              alt={name}
+              className="w-full h-full object-cover rounded-lg"
+            />
+          </Link>
+        </div>
+        <div className="flex flex-col">
+          <h3 className="pt-2 pl-2 font-bold text-md">{name}</h3>
+          <div className="flex items-center justify-between pl-2">
+            {character ? (
+              <div className="flex flex-col">
+                <p>{character}</p>
+              </div>
+            ) : known_for && known_for.length > 0 ? (
+                <div className="flex flex-wrap text-sm">
+                <p>{known_for[0].title !== undefined ? `${known_for[0].title}` : null }</p>
+                <p>{known_for[0].title !== undefined && known_for[1].title !== undefined  ? ',' : null }&nbsp;</p>
+                <p>{known_for[1].title !== undefined ? `${known_for[1].title}` : null }</p>
+              </div>
+            ) : null}
+          </div>
+        </div>
+      </div>
+    );
 }
