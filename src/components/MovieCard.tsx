3bcfd09bb7ac047b68d7a501ad65ada1ca50e21d
Kyle Thornton <kylemthornton@gmail.com>
2023-12-14 10:37:13 -0800

    Move rating

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -12,11 +12,11 @@ type MovieCardProps = {
 export const MovieCard: React.FC<MovieCardProps> = ({ poster, title, releaseDate, voteAverage, id }) => {
     const moviePoster = poster ? `https://image.tmdb.org/t/p/original/${poster}` : "/images/noposter.jpg"
     return (
-        <div className="flex flex-col m-2 w-96 h-96">
+        <div className="flex flex-col m-2 w-96 h-auto border rounded-xl pb-2">
             <div className="h-60 w-48 hover:scale-105 transition-all duration-200 ease-in-out cursor-pointer">
                 <Link href={`/movie/${id}`}><img src={moviePoster} alt={title} className="w-full h-full object-cover rounded-lg" /></Link>
             </div>
-            <div className="flex items-center justify-between pt-5">
+            <div className="flex justify-between pt-5 px-2">
                 <div>
                     <h3 className="font-bold text-md">{title}</h3>
                     <p>{releaseDate.slice(5,7)}/{releaseDate.slice(8,10)}/{releaseDate.slice(0,4)}</p>
