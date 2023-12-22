eb7087ffd9eed9d89cb5c6c69aa3ff6f32f9b3fe
Kyle Thornton <kylemthornton@gmail.com>
2023-12-21 20:54:45 -0800

    Fix spacing and add release date conditional

--- src/components/TVCard.tsx
+++ src/components/TVCard.tsx
@@ -12,14 +12,14 @@ type TVCardProps = {
 export const TVCard: React.FC<TVCardProps> = ({ poster, title, releaseDate, voteAverage, id }) => {
     const TVPoster = poster ? `https://image.tmdb.org/t/p/original/${poster}` : "/images/noposter.jpg"
     return (
-        <div className="flex flex-col m-2 w-96 h-auto border rounded-xl pb-2">
+        <div className="flex flex-col m-2 w-48 h-auto border rounded-xl pb-2">
             <div className="h-60 w-48 hover:scale-105 transition-all duration-200 ease-in-out cursor-pointer">
                 <Link href={`/tv/${id}`}><img src={TVPoster} alt={title} className="w-full h-full object-cover rounded-lg" /></Link>
             </div>
             <div className="flex justify-between pt-5 px-2">
                 <div>
                     <h3 className="font-bold text-md">{title}</h3>
-                    <p>{releaseDate.slice(5,7)}/{releaseDate.slice(8,10)}/{releaseDate.slice(0,4)}</p>
+                    {releaseDate ? <p>{releaseDate.slice(5,7)}/{releaseDate.slice(8,10)}/{releaseDate.slice(0,4)}</p> : null}
                 </div>
                 <div className="">
                         {voteAverage ? <Rating review={voteAverage} /> : null}
