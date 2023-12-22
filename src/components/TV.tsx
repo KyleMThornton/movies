ef8996231aa07c3835743ddfe4fa4efa46137d04
Kyle Thornton <kylemthornton@gmail.com>
2023-12-22 12:27:30 -0800

    Add bg color to TV

--- src/components/TV.tsx
+++ src/components/TV.tsx
@@ -5,20 +5,22 @@ export default async function TV() {
     const tvData = await response.json()
 
     return (
-        <div className="container text-black dark:text-white pb-4">
-            <h2 className="text-4xl font-bold pl-2 py-2">Popular Television</h2>
-            <div className="flex overflow-auto w-full custom-scrollbar">
-                {tvData.results.map((tv: any) => (
-                    <TVCard
-                        id={tv.id}
-                        key={tv.id}
-                        poster={tv.poster_path}
-                        title={tv.name}
-                        releaseDate={tv.first_air_date}
-                        voteAverage={tv.vote_average}
-                    />
-                )
-                )}
+        <div className="bg-slate-200">
+            <div className="container text-black dark:text-white pb-4">
+                <h2 className="text-4xl font-bold pl-2 py-2">Popular Television</h2>
+                <div className="flex overflow-auto w-full custom-scrollbar">
+                    {tvData.results.map((tv: any) => (
+                        <TVCard
+                            id={tv.id}
+                            key={tv.id}
+                            poster={tv.poster_path}
+                            title={tv.name}
+                            releaseDate={tv.first_air_date}
+                            voteAverage={tv.vote_average}
+                        />
+                    )
+                    )}
+                </div>
             </div>
         </div>
     )
