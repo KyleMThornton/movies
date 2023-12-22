5ca07011d00793ab57a7e3f7003282df137b7093
Kyle Thornton <kylemthornton@gmail.com>
2023-12-21 20:26:51 -0800

    Change home icon to text

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import Link from 'next/link';
-import { FaHome } from "react-icons/fa";
 import { useState, useRef } from "react";
 
 
@@ -17,11 +16,11 @@ export default function NavBar() {
         <div className='bg-slate-800'>
             <nav className='container flex flex-col md:flex-row justify-between p-8 items-center'>
                 <ul className='flex cursor-pointer pb-5 md:pb-0 text-slate-50 items-center'>
-                    <span className='px-2 text-xl'><Link href="/"><FaHome /></Link></span>
+                    <Link href="/"><li className='px-2 text-xl hover:font-bold'>Home</li></Link>
                     <Link href="/search/movie"><li className='px-2 text-xl hover:font-bold'>Movies</li></Link>
                     <Link href="/search/tv"><li className='px-2 text-xl hover:font-bold'>TV</li></Link>
                     <Link href="/search/person"><li className='px-2 text-xl hover:font-bold'>People</li></Link>
-                    <li className='px-2 text-xl hover:font-bold'>Dark/Light</li>
+                    {/* <li className='px-2 text-xl hover:font-bold'>Dark/Light</li> */}
                 </ul>
                 <div className='flex join'>
                     <input type="text" ref={searchbarRef} onChange={handleInputUpdate} className='input input-bordered join-item wd-1/3 md:w-80' />
