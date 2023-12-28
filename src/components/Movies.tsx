7427303b2ecab256dc6b8b8820eafc485d361a5e
Kyle Thornton <kylemthornton@gmail.com>
2023-12-28 14:24:44 -0800

    Add dark mode styling

--- src/components/Movies.tsx
+++ src/components/Movies.tsx
@@ -5,20 +5,22 @@ export default async function Movies() {
     const movieData = await response.json()
 
     return (
-        <div className="container py-5 text-black dark:text-white">
-            <h2 className="text-4xl font-bold p-2">Popular Movies</h2>
-            <div className="flex overflow-auto w-full custom-scrollbar">
-                {movieData.results.map((movie: any) => (
-                    <MovieCard
-                        key={movie.id}
-                        id={movie.id}
-                        poster={movie.poster_path}
-                        title={movie.title}
-                        releaseDate={movie.release_date}
-                        voteAverage={movie.vote_average}
-                    />
-                )
-                )}
+        <div className="bg-slate-100 dark:bg-slate-900">
+            <div className="container py-5 text-black dark:text-white">
+                <h2 className="text-4xl font-bold p-2">Popular Movies</h2>
+                <div className="flex overflow-auto w-full custom-scrollbar">
+                    {movieData.results.map((movie: any) => (
+                        <MovieCard
+                            key={movie.id}
+                            id={movie.id}
+                            poster={movie.poster_path}
+                            title={movie.title}
+                            releaseDate={movie.release_date}
+                            voteAverage={movie.vote_average}
+                        />
+                    )
+                    )}
+                </div>
             </div>
         </div>
     )
