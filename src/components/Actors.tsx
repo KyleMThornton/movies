f05fec923b2437d87b280952e362527c4923e2ee
Kyle Thornton <kylemthornton@gmail.com>
2023-12-28 14:24:29 -0800

    Add dark mode styling

--- src/components/Actors.tsx
+++ src/components/Actors.tsx
@@ -5,19 +5,21 @@ export default async function Actors() {
     const actorData = await response.json()
 
     return (
-        <div className="container pb-4 text-black dark:text-white">
-            <h2 className="text-4xl font-bold p-2">Popular Actors</h2>
-            <div className="flex overflow-auto w-full custom-scrollbar">
-                {actorData.results.map((actor: any) => (
-                    <ActorCard
-                        key={actor.id}
-                        id={actor.id}
-                        name={actor.name}
-                        profile_path={actor.profile_path}
-                        known_for={actor.known_for}
-                    />
-                )
-                )}
+        <div className="bg-slate-100 dark:bg-slate-900">
+            <div className="container pb-4 text-black dark:text-white">
+                <h2 className="text-4xl font-bold p-2">Popular Actors</h2>
+                <div className="flex overflow-auto w-full custom-scrollbar">
+                    {actorData.results.map((actor: any) => (
+                        <ActorCard
+                            key={actor.id}
+                            id={actor.id}
+                            name={actor.name}
+                            profile_path={actor.profile_path}
+                            known_for={actor.known_for}
+                        />
+                    )
+                    )}
+                </div>
             </div>
         </div>
     )
