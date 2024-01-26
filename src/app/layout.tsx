a4d9ea3613d2cb14eee8e17df4274126c7b46971
KyleMThornton <kylemthornton@gmail.com>
2024-01-26 15:16:51 -0800

    Add ubuntu font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,25 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-import NavBar from '@/components/NavBar'
-import Footer from '@/components/Footer'
+import type { Metadata } from "next";
+import { Ubuntu } from "next/font/google";
+import "./globals.css";
+import NavBar from "@/components/NavBar";
+import Footer from "@/components/Footer";
 import { Providers } from "./providers";
 
-const inter = Inter({ subsets: ['latin'] })
+const ubuntu = Ubuntu({ weight: ["400"], subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Entertainment Database',
-  description: 'Powered by Next.js, Tailwind CSS, and The Movie Database API.',
-}
+  title: "Entertainment Database",
+  description: "Powered by Next.js, Tailwind CSS, and The Movie Database API.",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={ubuntu.className}>
         <Providers>
           <NavBar />
           {children}
